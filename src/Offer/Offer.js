c494daa4bef049a7d0c7f54fbcc619c1713922f4
agu <iba.agustin@gmail.com>
2022-10-21 21:25:51 -0300

    make offer component main idea

--- src/Offer/Offer.js
+++ src/Offer/Offer.js
@@ -5,3 +5,7 @@ import { Title } from "../Shared/Title";
 export default function Offer() {
   <Wrapper></Wrapper>;
 }
+
+// Make offer requests
+// These offers go on the users client info
+// It can receive some already selected products (by the user) from the Home component
