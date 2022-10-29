8e68caff92d5f1a692bc891a7cf6bbf39354f261
agu <iba.agustin@gmail.com>
2022-10-29 20:01:27 -0300

    Toastalert component

---
+++ src/Shared/ToastAlert.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Alert = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Roboto, sans-serif;
+  font-weight: 500;
+  border: solid 1px;
+  border-radius: 4px;
+`;
