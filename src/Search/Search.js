f167fbaecd174f87984c777a0b8d427412fc020b
agu <iba.agustin@gmail.com>
2022-11-03 19:14:56 -0300

    products filtered and listed

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Wrapper } from "../Shared/Wrapper";
 import SearchBar from "../Shared/SearchBar";
 import { GetProducts } from "../Context/UserContext";
+import ListedProducts from "../Shared/ListedProducts";
 // label={props.label}
 // placeholder={props.placeholder}
 // type={props.text}
@@ -12,7 +13,30 @@ import { GetProducts } from "../Context/UserContext";
 
 export default function Search() {
   const [inputField, setInputField] = React.useState("");
+
   const [shownProducts, setShownProducts] = React.useState([]);
+
+  const [searchProduct, setSearchProduct] = React.useState({
+    type: "",
+    quantity: "",
+    unit: "",
+    productId: "",
+    description: "",
+    pictures: "asd",
+    badges: [],
+  });
+
+  const selectItem = (e) => {
+    setSearchProduct((oldProduct) => {
+      return {
+        ...oldProduct,
+        productId: e.target.id,
+        badges: [e.target.textContent],
+      };
+    });
+  };
+
+  // Renderizar ListedProducts debajo, pasandole filteredProducts como 'filteredProducts'
 
   const products = GetProducts();
   return (
@@ -24,6 +48,12 @@ export default function Search() {
         placeholder="ej: frutilla, apio, zanahoria..."
         label="Haz tu búsqueda!"
       ></SearchBar>
+      <ListedProducts
+        addItem={selectItem}
+        inputField={inputField}
+        filteredProducts={shownProducts}
+        display="flex"
+      ></ListedProducts>
     </Wrapper>
   );
 }
