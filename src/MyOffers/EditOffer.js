b51653e806ed7cbef5f088c4f0d353aef6f50020
agu <iba.agustin@gmail.com>
2022-11-10 16:43:40 -0300

    editoffer modal setup

---
+++ src/MyOffers/EditOffer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Wrapper } from "../Shared/Wrapper";
+import { FilterInput } from "../Shared/FilterInput";
+import ListedProducts from "../Shared/ListedProducts";
+import { BadgesWrapper, DetailsWrapper } from "../Shared/BadgeWrapper";
+import { InputQty } from "../Shared/InputField";
+import Select from "../Shared/Select/Select";
+
+// popup modal with options to edit the offer (probably not editing product)
+// but editing quantities, description, etc
+
+export default function EditOffer() {
+  return <Wrapper></Wrapper>;
+}
