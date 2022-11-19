b68d31465b43ba00bf420279a4673a935f0506fa
agu <iba.agustin@gmail.com>
2022-11-18 23:39:34 -0300

    generic helper fetch 1

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -5,7 +5,7 @@ const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
 // id, method, params, process data
-const editOffer = async (id, method, params) => {
+const editOffer = async (id, path, method, params, fn) => {
   const reqData = JSON.stringify(params);
 
   const reqOptions = {
@@ -15,4 +15,16 @@ const editOffer = async (id, method, params) => {
     redirect: "follow",
     credentials: "include",
   };
+
+  await fetch(`https://peka-api-wt2x.onrender.com/${path}`, reqOptions).then(
+    (response) =>
+      response
+        .text()
+        .then((result) => {
+          const data = JSON.parse(result);
+          console.log(data);
+          fn(data);
+        })
+        .catch((err) => console.log("error", err))
+  );
 };
